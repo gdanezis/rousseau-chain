cf14c0ecd46b1ed54e41e33cd354a39dfd2817f6
George D <georged@gmail.com>
2015-12-25 16:35:39 +0000

    Added first ideas for efficient chain

---
+++ chain.py
@@ -0,0 +1,124 @@
+# Make a hash chain with O(1) update and O(log(N)) proof of membership
+
+from hashlib import sha256 as H
+from struct import pack
+
+# Some constants
+
+# The initial value of any chain
+# https://en.wikipedia.org/wiki/Om
+initialH = H("Om").digest()
+
+def pointFingers(seqLen):
+    """ Returns the indexes for a paricular sequence ID """
+    seq = 1
+    while seq < seqLen:
+        yield seqLen - seq
+        seq = seq * 2
+
+class chain(object):
+
+    def __init__(self):
+        """ Create a new chain object """
+        # This holds the actual log entries
+        # it is a sequnence of byte arrays
+        self.entries = []
+
+        # The hash corresponding to the previous hash
+        self.currentHash = initialH
+
+        # The list of 'nodes' holding hashes of the current entry,
+        # and a sequence of previous node hashes.
+        self.nodes = []
+
+    def head(self):
+        """ Return the head of the chain """
+        if self.nodes == []:
+            return initialH
+        else:
+            return self.nodes[-1]
+
+    def add(self, entry):
+        """ Add an entry at the end of the chain. Returns the index of the new entry. """
+
+        # Create the new node head:
+        entryH = H(entry).digest()
+
+        nodeDigest = H(pack("L", len(self.entries)))
+        nodeDigest.update(entryH)
+
+        # Gather which other nodes are to be included:
+        for i in pointFingers(len(self.entries)):
+            nodeDigest.update(self.nodes[i])
+
+        nodeH = nodeDigest.digest()
+
+        self.entries += [entryH]
+        self.nodes += [nodeH]
+
+        return len(self.entries) - 1
+
+    def evidence(self, seq):
+        """ Gather evidence that the entry is at a sequence number in the chain. """
+        entries = {}
+        nodes = {}
+
+        # Add evidence as required
+        target = len(self.entries) - 1
+        while seq not in entries:
+
+            # Store the entry for the current target
+            entries[target] = self.entries[target]
+            nodes[target] = self.nodes[target]
+
+            # Store the nodes on which we depend
+            for i in pointFingers(target):
+                nodes[i] = self.nodes[i]
+                if i >= seq:
+                    target = i
+
+        # Return all necessary entries and nodes
+        return entries, nodes
+
+
+def check_evidence(head, seq, evidence, entry=None, node=None):
+    """ Check that a bundle of evidence is correct, and correspond to,
+    a known head, and optionaly a known entry and known node. Returns
+    True or raises an exception. """
+    entries, nodes = evidence
+    head_index = max(entries.keys())
+
+    # CHECK 1: the head equals the head
+    if not (head == nodes[head_index]):
+        raise Exception("Wrong Head")
+
+    # CHECK 2: all the hashes match
+    target = head_index
+    while target != seq:
+
+        new_target = target
+
+        # Make the digest
+        d = H(pack("L", target))
+        d.update(entries[target])
+        for i in pointFingers(target):
+            d.update(nodes[i])
+            if i >= seq:
+                new_target = i
+
+        if d.digest() != nodes[target]:
+            raise Exception("Broken Chain")
+
+        target = new_target
+
+    # CHECK 3: is the node correct?
+    if node:
+        if not (node == nodes[seq]):
+            raise Exception("Wrong end node")
+
+    # CHECK 4: is the actual entry correct?
+    if entry:
+        if not (H(entry).digest() == entries[seq]):
+            raise Exception("Wrong end entry")
+
+    return True
---
+++ test_chain.py
@@ -0,0 +1,74 @@
+from chain import chain, check_evidence, pointFingers, initialH
+
+# The unit tests, because we are professionals
+# run with 'py.test -sv .'
+def test_create():
+    c = chain()
+    assert c.head() == initialH
+
+    assert c.add("Hello") == 0
+
+def test_check():
+    c = chain()
+    c.add("Hello")
+
+    # Add a lot of those
+    for _ in xrange(1000):
+        c.add("Hello")
+
+    # Produce and check evidence
+    e = c.evidence(100)
+    assert check_evidence(c.head(), 100, e)
+    assert check_evidence(c.head(), 100, e, entry="Hello")
+
+def test_check_negative():
+    c = chain()
+    c.add("Hello")
+
+    # Add a lot of those
+    for _ in xrange(1000):
+        c.add("Hello")
+
+    e = c.evidence(100)
+
+    import pytest
+    # Wrong entry
+    with pytest.raises(Exception):
+        check_evidence(c.head(), 100, e, entry="Hello2")
+
+    # Wrong nodes
+    with pytest.raises(Exception):
+        check_evidence(c.head(), 100, e, node="Hello2")
+
+    # Wrong head
+    with pytest.raises(Exception):
+        check_evidence(c.nodes[-2], 100, e)
+
+    # Wrong chain
+    (en, no) = e
+    no[88] = no[231]
+    with pytest.raises(Exception):
+        check_evidence(c.head(), 100, (en, no))
+
+def test_ensure_prefix():
+    c = chain()
+    c.add("Hello")
+
+    # Add a lot of those
+    for _ in xrange(1000):
+        c.add("Hello")
+
+    seq = c.add("hello")
+    head = c.head()
+
+    for _ in xrange(1000):
+        c.add("Hello")
+
+    new_head = c.head()
+
+    e = c.evidence(seq)
+    assert check_evidence(new_head, seq, e, node=head)
+
+def test_pointFingers():
+    assert list(pointFingers(16)) == [15,14, 12, 8]
+    assert len(list(pointFingers(256))) == 8
