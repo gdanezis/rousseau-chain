8ae7edf4ded8d59a28a803176df502c91d5c91a4
gdanezis <g.danezis@ucl.ac.uk>
2015-12-29 14:20:10 +0000

    Separating store from client

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from kafka import SimpleProducer, KafkaClient
 from kafka import KafkaConsumer
 
 from chain import chain
+from store import diskHashList
 
 import logging
 from binascii import hexlify
@@ -11,36 +12,6 @@ logging.basicConfig(
     level=logging.WARNING
 )
 
-class diskHashList:
-    def __init__(self, filename):
-        self.f = open(filename, "r+b")
-
-    def append(self, value):
-        assert type(value) == bytes and len(value) == 32
-        self.f.seek(0, 2) # End of the file
-        self.f.write(value)
-        self.f.flush()
-
-    def __len__(self):
-        self.f.seek(0,2) # move the cursor to the end of the file
-        size = self.f.tell()
-        return size / 32
-
-    def __getitem__(self, key):
-        size = len(self)
-
-        if key < 0:
-            key = size + key
-
-        assert type(key) == int and 0 <= key
-        self.f.seek(32 * key, 0)
-        data = self.f.read(32)
-        assert len(data) == 32
-        return data
-
-    def __del__(self):
-        self.f.flush()
-        self.f.close()
 
 def main():
     ## Register to read messages from the "rousseau" list
---
+++ store.py
@@ -0,0 +1,62 @@
+from collections import Sequence
+from os.path import exists
+from os import remove
+
+class diskHashList(Sequence):
+    def __init__(self, filename):
+        if not exists(filename):
+            fx = open(filename, "w+b")
+            fx.close()
+        self.f = open(filename, "r+b")
+
+    def append(self, value):
+        assert type(value) == bytes and len(value) == 32
+        self.f.seek(0, 2) # End of the file
+        self.f.write(value)
+        self.f.flush()
+
+    def __len__(self):
+        self.f.seek(0,2) # move the cursor to the end of the file
+        size = self.f.tell()
+        return size / 32
+
+    def __getitem__(self, key):
+        size = len(self)
+
+        if key < 0:
+            key = size + key
+
+        assert type(key) == int and 0 <= key
+        self.f.seek(32 * key, 0)
+        data = self.f.read(32)
+        assert len(data) == 32
+        return data
+
+    def __del__(self):
+        self.f.flush()
+        self.f.close()
+
+def test_direct_operations():
+    s = diskHashList("test_store.dat")
+    assert len(s) == 0
+
+    s.append(b"A" * 32)
+    s.append(b"B" * 32)
+    s.append(b"C" * 32)
+
+    assert len(s) == 3
+    assert s[0] == b"A" * 32
+    assert s[1] == b"B" * 32
+    assert s[2] == b"C" * 32
+
+    remove("test_store.dat")
+
+def test_indirect_operations():
+    s = diskHashList("test_store.dat")
+    assert len(s) == 0
+
+    s.append(b"A" * 32)
+    s.append(b"B" * 32)
+    s.append(b"C" * 32)
+
+    assert s[:2] == [b"A" * 32, b"B" * 32]
