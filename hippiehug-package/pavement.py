34fbe38ee70c8257f6258d002ea9d027a037301d
gdanezis <g.danezis@ucl.ac.uk>
2016-03-05 19:12:15 +0000

    Added some automation scripts

---
+++ hippiehug-package/pavement.py
@@ -0,0 +1,39 @@
+import os.path
+import os
+import re
+import fnmatch
+
+from paver.tasks import task, cmdopts
+from paver.easy import sh, needs, pushd
+from paver.virtual import *
+
+@task
+def test():
+    """ Run all the unit tests in a generic py.test context. """
+    print("Generic Unit tests")
+    sh('py.test -vs --doctest-modules tests/test_*.py')
+
+@task
+def build(quiet=True):
+    """ Builds the distribution, ready to be uploaded to pypi. """
+    print("Build dist")
+    sh('python setup.py sdist bdist bdist_wheel', capture=quiet)
+
+@task
+def win(quiet=True):
+    """ Builds the binary distribution for windows. """
+    print("Build windows distribution")
+    sh('python setup.py build bdist_wininst', capture=quiet)
+
+@task
+def upload(quiet=False):
+    """ Uploads the latest distribution to pypi. """
+    pass
+
+    lib = file(os.path.join("hippiehug", "__init__.py")).read()
+    v = re.findall("VERSION.*=.*['\"](.*)['\"]", lib)[0]
+
+    print("upload dist: %s" % v)
+    sh('python setup.py sdist bdist')
+    sh("twine upload dist/*%s*" % v)
+    
