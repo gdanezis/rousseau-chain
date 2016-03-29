e834742ce856a54a2f6b5e2335f8cba8342a99e4
gdanezis <g.danezis@ucl.ac.uk>
2016-03-29 22:36:21 +0100

    Added non-recoverable support for Kafka

--- rousseau-package/KafkaConsensus.py
+++ rousseau-package/KafkaConsensus.py
@@ -15,8 +15,9 @@ from threading import Thread
 from consensus import Node, packageTx, within_TX
 # import redis
 
-from kafka import SimpleProducer, KafkaClient
-from kafka import KafkaConsumer
+# from kafka import SimpleProducer, KafkaClient
+# from kafka import KafkaConsumer
+from kafka import KafkaClient, SimpleProducer, SimpleConsumer
 
 logging.basicConfig(
     format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
@@ -25,21 +26,16 @@ logging.basicConfig(
 
 class Listener(Thread):
     """ Helper function that monitors a channel and fires receive events. """
-    def __init__(self, kafka_node, addr, channels):
+    def __init__(self, node, channel):
         Thread.__init__(self)
-        self.node = kafka_node
+        self.node = node
         self.daemon = True
 
-        assert len(channels) == 1
-        channel = channels[0]
-        self.consumer = KafkaConsumer(channel, group_id='consensus',
-                         bootstrap_servers=['%s:%s' % addr])
-
-        # self.node.r.pubsub()
-        # self.pubsub.subscribe(channels)
+        self.consumer = SimpleConsumer(self.node.kafka, 'consensus', channel)
     
     def work(self, item):
         try:
+            print "Process:", item
             self.node.receive(item)
         except Exception as e:
             self.node.RLogger.info("Message Error: %s" % str(e))
@@ -47,33 +43,33 @@ class Listener(Thread):
     
     def run(self):
         for message in self.consumer:
-            self.work(message.value)
+            self.work(message.message.value)
 
     def teardown(self):
         self.node.RLogger.info("Tear down PubSub (%s)" % self.node.name)
-        #self.pubsub.unsubscribe()
-        pass
-
+        
 
 class KafkaNode(Node):
     """ A consensus node based on Redis Pub/Sup. """
 
     def __init__(self, start = [], quorum=1, name = None, 
             shard=None, shard_map=None,
-            host='localhost', port=9092, db=0):
-        self.r = KafkaClient('%s:%s' % (host,port)) # redis.StrictRedis(host, port, db)
+            host='localhost', port=9092):
+        self.client = None
+
+        self.kafka = KafkaClient('%s:%s' % (host,port)) # redis.StrictRedis(host, port, db)
+        self.producer = SimpleProducer(self.kafka, async=True)
+
         self.shard_id = shard
         self.shard_map = shard_map
-
-        addr = (host, port)
 
         # Configure the shard
         if shard is not None:
             assert shard in shard_map
             shard = shard_map[shard]
-            self.client = Listener(self, addr, ['votes:%s' % self.shard_id])
+            self.client = Listener(self, 'votes:%s' % self.shard_id)
         else:
-            self.client = Listener(self, addr, ['votes'])
+            self.client = Listener(self, 'votes')
 
         # Initialize the node for consensus.
         Node.__init__(self, start, quorum, name, shard)
@@ -82,8 +78,12 @@ class KafkaNode(Node):
         self.client.start()
         self.RLogger = logging.getLogger()
 
+        print "Init done"
+
     def __del__(self):
-        self.client.teardown()
+        if self.client:
+            self.client.teardown()
+            self.kafka.close()
 
     def send(self, tx, msg):
         if self.shard_map is not None:
@@ -91,9 +91,9 @@ class KafkaNode(Node):
                 (b0, b1) = self.shard_map[i]
                 if within_TX(tx, b0, b1):
                     # self.r.publish('votes:%s' % i , msg)
-                    self.r.send_messages('votes:%s' % i , msg)
+                    self.producer.send_messages('votes:%s' % i , msg)
         else:
-            self.r.send_messages('votes' , msg)
+            self.producer.send_messages('votes' , msg)
 
     def receive(self, message):
         """ How to process incoming messages. """
@@ -161,3 +161,30 @@ class KafkaNode(Node):
         self.send(full_tx, msg)
 
 
+if __name__ == "__main__":
+    host = "ec2-54-171-154-70.eu-west-1.compute.amazonaws.com"
+    port = 9092
+
+    kn = KafkaNode(name="node", host=host, port=port)
+
+    rnd = hexlify(urandom(16))
+    T0 = packageTx(data="data1,%s" % rnd, deps=[], num_out=3)
+    _, _, [A, B, C], txdata = T0
+
+    T0_json = dumps({ "action":"process", "from":"ext", "Tx":T0 })
+    kn.send(T0, T0_json)
+
+    T1 = packageTx("333,%s" % rnd, [A, B], 2)
+    T2 = packageTx("bbb,%s" % rnd, [A, C], 2)
+    T1_json = dumps({ "action":"process", "from":"ext", "Tx":T1 })
+    T2_json = dumps({ "action":"process", "from":"ext", "Tx":T2 })
+
+    kn.send(T1, T1_json)
+    kn.send(T2, T2_json)
+
+    try:
+        while True:
+            time.sleep(1.0)
+    except KeyboardInterrupt:
+        del kn
+        print "Clean exit ..."
