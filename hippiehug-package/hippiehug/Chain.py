595154aab0f43d43a1b1a5369d37773e36a99938
Bogdan Kulynych <hello@bogdankulynych.me>
2017-06-05 15:41:35 +0200

    Pepify, docs, utils

--- hippiehug-package/hippiehug/Chain.py
+++ hippiehug-package/hippiehug/Chain.py
@@ -1,22 +1,18 @@
 from copy import copy
 from binascii import hexlify
+from msgpack import packb
 
-from .Nodes import h as binary_hash
+from hippiehug.Utils import binary_hash
 
-def ascii_hash(s):
-    return hexlify(binary_hash(s)[:20])
-
-try:
-    from msgpack import packb
-except:
-    print("No msgpack")
 
 def get_fingers(seq):
     return set(seq - 1 - ((seq - 1) % (2**f)) for f in range(64))
 
+
 def check_hash(key, val):
     if key != val.hid: # val.identity():
         raise Exception("Value has the wrong hash.")
+
 
 class Document:
     __slots__ = ["item", "hid"]
@@ -25,7 +21,7 @@ class Document:
         self.item = item
         """ The item stored in the Leaf. """
 
-        self.hid = ascii_hash(packb(("D",self.item)))
+        self.hid = binary_hash(packb(("D", self.item)))
 
     def identity(self):
         """ Returns the hash ID of the Leaf. """
@@ -34,75 +30,83 @@ class Document:
     def __eq__(self, other):
         return self.hid == other.hid
 
+
 class Block:
-    def __init__(self, items, sequence=0, fingers=[]):
-        """ Initialize a block. """
-        self.sequence = sequence
-        self.items = items  
-        self.fingers = fingers
-        self.length = len(items)
+    def __init__(self, items, index=0, fingers=None):
+        """Initialize a block."""
+        self.items = items
+        self.index = index
+        self.fingers = fingers or []
 
         # Precomute the head
         self.hid = self.head()
 
     def head(self):
-        """ Returns the head of the block. """
-        return ascii_hash(packb(("S", self.sequence, self.fingers, self.length, self.items)))
+        """Return the head of the block."""
+        return binary_hash(packb(("S", self.index, self.fingers, self.items)))
 
     def next_block(self, store, items):
-        """ Builds a subsequent block, selaing a list of transactions. """
-        new_sequence = self.sequence + 1 
-        new_fingers = [ (self.sequence, self.hid) ]
+        """Build a subsequent block, sealing a list of transactions.
 
-        finger_index = get_fingers(new_sequence)
+        :param store: Backend
+        :param items: Block items
+        """
+        new_index = self.index + 1
+        new_fingers = [(self.index, self.hid)]
+
+        finger_index = get_fingers(new_index)
         new_fingers += [f for f in self.fingers if f[0] in finger_index]
 
-        new_b = Block(items, new_sequence, new_fingers)
+        new_b = Block(items, new_index, new_fingers)
         store[new_b.hid] = new_b
         return new_b
 
-    def get_item(self, store, block_seq, item_seq, evidence = None):
-        """ Returns an iten from the chain, at a specific block and item ID.
-        Optionally returns a bundle of evidence. """
-        # print "FIND: %s (%s, %s)" % (self.sequence, block_seq, item_seq)
+    def get_item(self, store, block_index, item_index, evidence=None):
+        """Return an item from the chain at a specific block and item index.
 
-        if not (0 <= block_seq <= self.sequence):
-            raise Exception("Block is beyond this chain head: must be 0 <= %s <= %s." % (block_seq, self.sequence))
+        :param store: Backend
+        :param block_index: Block index
+        :param item_index: Item index
+        :param evidence: If not None, return a bundle of evidence
+        """
+        # print "FIND: %s (%s, %s)" % (self.index, block_index, item_index)
+
+        if not (0 <= block_index <= self.index):
+            raise Exception("Block is beyond this chain head: must be 0 <= %s <= %s." % (block_index, self.index))
 
         if evidence != None:
             evidence[self.hid] = self
 
-        if block_seq == self.sequence:
+        if block_index == self.index:
+            if not (0 <= item_index < len(self.items)):
+               raise Exception("Item is beyond this Block: must be 0 <= %s <= %s." % (item_index, len(self.items)))
 
-            if not (0 <= item_seq < self.length):
-               raise Exception("Item is beyond this Block: must be 0 <= %s <= %s." % (item_seq, self.length))
+            return self.items[item_index]
 
-            return self.items[item_seq]
-
-        _, target_h = [(f,block_hash) for (f, block_hash) in self.fingers if f >= block_seq][-1]
+        _, target_h = [(f,block_hash) for (f, block_hash) in self.fingers if f >= block_index][-1]
 
         # Get the target block and check its integrity
         target_block = store[target_h]
         check_hash(target_h, target_block)
 
-        return target_block.get_item(store, block_seq, item_seq, evidence)
+        return target_block.get_item(store, block_index, item_index, evidence)
 
     def __eq__(self, other):
         return self.hid == other.hid
 
 
 class Chain:
-    def __init__(self, store = {}, root_hash = None):
-        """ Initializes a chained backed by a store. """
-        self.store = store
+    def __init__(self, store=None, root_hash=None):
+        """Initialize a chain backed by a store."""
+        self.store = store or {}
         self.head = root_hash
 
     def root(self):
-        """ Returns the head of the chain. """
+        """Return the head of the chain."""
         return self.head
 
     def multi_add(self, items):
-        """ Adds a batch of elements and seals a new block. """
+        """Add a batch of elements and seal a new block."""
         if self.head is None:
             # Make a new one
             b0 = Block( items )
@@ -114,8 +118,8 @@ class Chain:
             self.head = b1.hid
 
     def get(self, block_index, item_index, evidence=None):
-        """ Returns the record at a specific block an item ID, 
-            and potentially a bundle of evidence. """
+        """Return the record at a specific block and item index,
+        and potentially a bundle of evidence. """
         if self.head is None:
             return None
 
@@ -125,11 +129,12 @@ class Chain:
 
         return last_block.get_item(self.store, block_index, item_index, evidence)
 
+
 class DocChain(Chain):
-    ''' A chain that stores hashes of documents. Construct like a *Chain*. '''
+    """A chain that stores hashes of documents. Construct like a *Chain*."""
 
     def multi_add(self, items):
-        ''' Add multiple items to seal a new block. '''
+        """Add multiple items to seal a new block."""
 
         docs = list(map(Document, items))
         for d in docs:
@@ -139,20 +144,20 @@ class DocChain(Chain):
         Chain.multi_add(self, docs_id)
 
     def get(self, block_index, item_index, evidence=None):
-        ''' Get a sealed item, and optionally a bundle of evidence. '''
+        """Get a sealed item, and optionally a bundle of evidence."""
 
         ## Get Doc and check its hash
         item = Chain.get(self, block_index, item_index, evidence)
         d = self.store[item]
         check_hash(item, d)
-        
+
         if evidence != None:
             evidence[d.hid] = d
 
         return self.store[item].item
 
     def check(self, root, block_index, item_index, item):
-        """ Check that an item is within the structure at a specific point. """
+        """Check that an item is within the structure at a specific point."""
         ret = True
         ret = ret and (self.root() == root)
         ret = ret and (self.get(block_index, item_index) == item)
