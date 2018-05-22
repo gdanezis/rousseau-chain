e8f957f18dd4b7b167773a789070c8271d1ea405
holger krekel <holger@merlinux.eu>
2018-05-22 18:07:05 +0200

    prepare new release:
    - read version from hippiehug/__init__.py
    
    - include regular tests in tox run
    
    - ignore pytest cache files

--- hippiehug-package/hippiehug/__init__.py
+++ hippiehug-package/hippiehug/__init__.py
@@ -3,6 +3,7 @@ from .Chain import Chain, Block, DocChain
 from .RedisStore import RedisStore
 from .Nodes import h, Leaf, Branch
 
-VERSION = "0.0.10"
+__version__ = "0.0.10"
+VERSIN = __version__
 
 __all__ = ["Tree", "Chain", "DocChain", "RedisStore", "h"]
