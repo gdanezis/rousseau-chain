8587fd0deffee9c17e82c0cdf7b8627c6d45aca8
holger krekel <holger@merlinux.eu>
2018-05-22 18:07:05 +0200

    prepare new release:
    - read version from hippiehug/__init__.py
    
    - include regular tests in tox run

--- hippiehug-package/setup.py
+++ hippiehug-package/setup.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
 
-setup(name='hippiehug',
-      version='0.1.2',
-      description='A Merkle Tree implementation with a flexible storage backend.',
-      author='George Danezis',
-      author_email='g.danezis@ucl.ac.uk',
-      url=r'https://pypi.python.org/pypi/hippiehug/',
-      packages=['hippiehug'],
-      license="2-clause BSD",
-      long_description="""A Merkle Tree and hash chains implementation with a flexible storage backend.""",
 
-      test_require=["pytest >= 2.6.4"],
-      install_requires=[
-            "redis >= 2.0.0",
-            "future >= 0.14.3",
-            "pytest >= 2.6.4",
-            "msgpack-python >= 0.4.6",
-            "six >= 1.10.0"
-      ],
-      zip_safe=False,
-)
+def main():
+    with open(os.path.join("hippiehug", "__init__.py")) as f:
+        for line in f:
+            if "__version__" in line.strip():
+                version = line.split("=", 1)[1].strip().strip('"')
+                break
+        else:
+            raise RuntimeError("version not found")
+
+    setup(name='hippiehug',
+          version=version,
+          description='A Merkle Tree implementation with a flexible storage backend.',
+          author='George Danezis',
+          author_email='g.danezis@ucl.ac.uk',
+          url=r'https://pypi.python.org/pypi/hippiehug/',
+          packages=['hippiehug'],
+          license="2-clause BSD",
+          long_description="""A Merkle Tree and hash chains implementation with a flexible storage backend.""",
+
+          install_requires=[
+                "redis >= 2.0.0",
+                "future >= 0.14.3",
+                "msgpack-python >= 0.4.6",
+                "six >= 1.10.0"
+          ],
+          zip_safe=False,
+    )
+
+if __name__ == "__main__":
+    main()
