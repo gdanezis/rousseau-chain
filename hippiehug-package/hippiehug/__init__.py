881fe3e9aac89f42eb7877b480498f910aa37d22
holger krekel <holger.krekel@gmail.com>
2018-05-23 11:00:05 +0200

    prepare hippiehug-0.1.3 release (#7)
    * prepare new release:
    
    - read version from hippiehug/__init__.py
    
    - include regular tests in tox run
    
    - ignore pytest cache files
    
    - fix unicode/bytes issues with tests
    
    * add manifest

--- hippiehug-package/hippiehug/__init__.py
+++ hippiehug-package/hippiehug/__init__.py
@@ -3,6 +3,6 @@ from .Chain import Chain, Block, DocChain
 from .RedisStore import RedisStore
 from .Nodes import h, Leaf, Branch
 
-VERSION = "0.0.10"
+__version__ = "0.1.3"
 
 __all__ = ["Tree", "Chain", "DocChain", "RedisStore", "h"]
